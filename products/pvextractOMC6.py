55948e6bd6f814e0be8b94f72918d93067b34ead
Shuo Kong <shuo.kong@yale.edu>
2019-06-12 12:05:47 -0400

    improve pv

--- products/pvextractOMC6.py
+++ products/pvextractOMC6.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import numpy as np
 name_in = 'imsub_mask_imfit_c18o_pix_2_Tmb.fits'
 from pvextractor import Path
@@ -9,18 +10,24 @@ def findpath(pp,cutlen,cutdeg):
     """pp, cutlen in pix, cutdeg in degree from north to east"""
     return ([(pp[0]-cutlen/2.*np.cos(np.deg2rad(cutdeg)),pp[1]+cutlen/2.*np.cos(np.deg2rad(cutdeg))),(pp[0]+cutlen/2.*np.cos(np.deg2rad(cutdeg)),pp[1]-cutlen/2.*np.cos(np.deg2rad(cutdeg)))])
 
-pps = [(321.66161,338.17223),(314.8455,330.80769),(308.02937,323.44315),(301.5254,315.86686),(294.15632,308.97838),(286.94744,301.92969),(280.21915,294.40042),(273.33066,287.19153),(266.76257,279.66226),(260.35467,271.97278),(253.78658,264.44351),(246.7379,257.39482),(239.68921,250.18594),(232.80073,242.97706),(225.91224,235.76818),(218.86356,228.5593),(211.97507,221.35042),(204.76619,214.30173),(197.55731,207.41325),(190.66882,200.20436),(183.94053,192.67509),(176.73165,185.6264),(169.84316,178.25732),(163.11487,170.72805),(156.38658,163.35897),(149.81849,155.82969)]
+#pps = [(321.66161,338.17223),(314.8455,330.80769),(308.02937,323.44315),(301.5254,315.86686),(294.15632,308.97838),(286.94744,301.92969),(280.21915,294.40042),(273.33066,287.19153),(266.76257,279.66226),(260.35467,271.97278),(253.78658,264.44351),(246.7379,257.39482),(239.68921,250.18594),(232.80073,242.97706),(225.91224,235.76818),(218.86356,228.5593),(211.97507,221.35042),(204.76619,214.30173),(197.55731,207.41325),(190.66882,200.20436),(183.94053,192.67509),(176.73165,185.6264),(169.84316,178.25732),(163.11487,170.72805),(156.38658,163.35897),(149.81849,155.82969)]
+pps = [(314.8455,330.80769),(308.02937,323.44315),(301.5254,315.86686),(294.15632,308.97838),(286.94744,301.92969),(280.21915,294.40042),(273.33066,287.19153),(266.76257,279.66226),(260.35467,271.97278),(253.78658,264.44351),(246.7379,257.39482),(239.68921,250.18594),(232.80073,242.97706),(225.91224,235.76818),(218.86356,228.5593),(211.97507,221.35042),(204.76619,214.30173),(197.55731,207.41325),(190.66882,200.20436),(183.94053,192.67509),(176.73165,185.6264),(169.84316,178.25732),(163.11487,170.72805),(156.38658,163.35897)] # remove first and last
+
+makepvfits = 0
 
 cutlen = 200./2.
 cutdeg = 45
 name_outs = []
+if makepvfits == 1:
+    os.system('rm pvcuts/slice*.fits')
 for nn,pp in enumerate(pps):
-    pathi = findpath(pp,cutlen,cutdeg)
-    path1 = Path(pathi, width=10) # 10 pixels, 20 arcsec
-    slice1 = extract_pv_slice(name_in, path1)
     name_out = 'pvcuts/slice'+str(nn+1)+'_imsub_mask_imfit_c18o_pix_2_Tmb.fits'
     name_outs.append(name_out)
-    slice1.writeto(name_out,overwrite=True) 
+    if makepvfits == 1:
+        pathi = findpath(pp,cutlen,cutdeg)
+        path1 = Path(pathi, width=10) # 10 pixels, 20 arcsec
+        slice1 = extract_pv_slice(name_in, path1)
+        slice1.writeto(name_out,overwrite=True) 
 
 import matplotlib.pyplot as plt
 import pyfits
@@ -28,18 +35,18 @@ import os
 from matplotlib import gridspec
 from matplotlib import rc
 rc('text', usetex=True)
-font = {'weight' : 'normal','size':30,'family':'sans-serif','sans-serif':['Helvetica']}
+font = {'weight' : 'normal','size':20,'family':'sans-serif','sans-serif':['Helvetica']}
 rc('font', **font)
 
-xpanels = 1
-ypanels = 1
-xpanelwidth = 7
-ypanelwidth = 10
+xpanels = 4
+ypanels = 6
+xpanelwidth = 3
+ypanelwidth = 4
+os.system('rm pvcuts/omc6_pv*.pdf')
+pdfname='pvcuts/omc6_pv.pdf'
+fig=plt.figure(figsize=(xpanelwidth*xpanels*1.1,ypanelwidth*ypanels))
+plt.subplots_adjust(wspace=0.1,hspace=0.1)
 for nn,name_out in enumerate(name_outs):
-    pdfname='pvcuts/omc6_pv'+str(nn+1)+'.pdf'
-    fig=plt.figure(figsize=(xpanelwidth*xpanels*1.1,ypanelwidth*ypanels))
-    gs = gridspec.GridSpec(ypanels,xpanels,height_ratios=[1])
-    #plt.subplots_adjust(wspace=0.001,hspace=0.001)
     mmap=pyfits.open(name_out)
     cube=mmap[0].data.T
     cdelt1=mmap[0].header['cdelt2']/1000. # velocity in km/s
@@ -56,19 +63,26 @@ for nn,name_out in enumerate(name_outs):
     posticks = np.arange(0.,201.,20.)
     pospix = [(ii-crval2)/cdelt2+crpix2 for ii in posticks]
     postickslatex = [r"$"+str(int(ii))+r"$" for ii in posticks]
-    ax1 = plt.subplot(gs[0])
+    ax1 = fig.add_subplot(ypanels,xpanels,nn+1)
     ax1.imshow(cube,cmap='gray_r',aspect='auto',extent=[-0.5,naxis1-0.5,naxis2+1,1],interpolation='bicubic')
+    ax1.text(0.9, 0.9, str(nn+1),horizontalalignment='center',verticalalignment='center',transform = ax1.transAxes)
     ax1.set_xticks(velpix)
     ax1.set_xticklabels(veltickslatex)
+    if nn / xpanels == ypanels - 1:
+        ax1.set_xlabel(r'$\rm Velocity~(km~s^{-1})$')
     ax1.set_yticks(pospix)
     ax1.set_yticklabels(postickslatex)
+    if nn % xpanels == 0:
+        ax1.set_ylabel(r'$\rm Offsets~(arcsec)$')
+    if nn / xpanels < ypanels - 1:
+        plt.setp(ax1.get_xticklabels(),visible=False)
+    if nn % xpanels > 0:
+        plt.setp(ax1.get_yticklabels(),visible=False)
     ax1.tick_params(axis='both',direction='in',length=5,which='major',top=True,right=True)
-    ax1.set_xlabel(r'$\rm Velocity~(km~s^{-1})$')
-    ax1.set_ylabel(r'$\rm Offsets~(arcsec)$')
     
-    os.system('rm '+pdfname)
-    plt.savefig(pdfname,bbox_inches='tight')
-    plt.close(fig)
-    os.system('open '+pdfname)
-    os.system('cp '+pdfname+os.path.expandvars(' ~/GoogleDrive/imagesSFE/pvcuts/'))
+os.system('rm '+pdfname)
+plt.savefig(pdfname,bbox_inches='tight')
+plt.close(fig)
+os.system('open '+pdfname)
+os.system('cp '+pdfname+os.path.expandvars(' ~/GoogleDrive/imagesSFE/'))
 
